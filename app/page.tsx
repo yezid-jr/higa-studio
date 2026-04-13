39bf7ab6433f30fe6424d82b4857ebb729a527e8
Yesid Castro <castrogil202@gmail.com>
2026-04-13 16:22:01 -0500

    fix: add redirect to portfolio in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import AboutMe from "./components/AboutMe";
 import GalleryPreview from "./components/GalleryPreview";
 import Hero from "./components/Hero";
@@ -5,6 +6,7 @@ import NewsSlider from "./components/NewsSlider";
 import OptionsNav from "./components/OptionsNav";
 
 export default function Home() {
+  redirect("/portfolio");
   return (
     <main className="flex flex-col gap-3 bg-stone-100">
       {/* <Hero />
